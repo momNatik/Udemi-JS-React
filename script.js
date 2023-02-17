6f087a982bf5c65c2a8f9bcfa9bdecb9d62d1850
momNatik <bas.n@list.ru>
2023-02-17 21:51:18 +0300

    feat: first js code (19 lesson)

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use strict";
-let number = 5;
-const leftBorder = 1;
-console.log(number);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const lastFilm = prompt('Один из последних просмотренных фильмов', '');
+const lastFilmScore = prompt('На сколько оцените его?', '');
+const lastFilm2 = prompt('Один из последних просмотренных фильмов', '');
+const lastFilmScore2 = prompt('На сколько оцените его?', '');
+const personMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+personMovieDB.movies[lastFilm] = lastFilmScore;
+personMovieDB.movies[lastFilm2] = lastFilmScore2;
+console.log(personMovieDB);
