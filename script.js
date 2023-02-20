381b7b457a2de376daa2691b0b8d673c1bbc524d
momNatik <bas.n@list.ru>
2023-02-20 17:10:33 +0300

    feat: 21 lesson && ||

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ const personMovieDB = {
 personMovieDB.movies[lastFilm] = lastFilmScore;
 personMovieDB.movies[lastFilm2] = lastFilmScore2;
 console.log(personMovieDB);
+
+
+console.log( NaN || 2 || undefined );
+console.log( NaN && 2 && undefined );
+console.log( 1 && 2 && 3 );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( NaN || null || !3 || undefined || 5);
+console.log( NaN || null && !3 && undefined || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
