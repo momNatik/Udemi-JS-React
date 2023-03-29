fba0f16447adee095b5e2744fe29b6eb9182638f
momNatik <bas.n@list.ru>
2023-03-29 11:41:25 +0300

    refactor: using filter, join, ternary op., rename

--- array_35task.js
+++ array_35task.js
@@ -4,11 +4,11 @@
 const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
 function showFamily(arr) {
-    if (arr.length === 0) {
-        return 'Семья пуста';
-    } else {
-        return `Семья состоит из: ${arr.join(' ')}`;
-    }
+    const familyMembers = arr.join(' ');
+
+    return arr.length === 0
+        ? 'Семья пуста'
+        : `Семья состоит из: ${familyMembers}`;
 }
 
 console.log(showFamily(family));
@@ -44,30 +44,40 @@ console.log(reverse(someString));
 
 
 // 4) У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
-// нужно создать главную функцию банкомата, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу, второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'
+// нужно создать главную функцию банкомата, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу, второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют', если не пустой, то нужно возвращать список доступных валют (за исключением той, которая отсутствует)
 
 const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-let arr = baseCurrencies.concat(additionalCurrencies);
+const missingCurrency = 'EUR';
 
-function availableCurr(arr, missingCurr) {
+const allCurrencies = baseCurrencies.concat(additionalCurrencies);
+
+function availableCurr(currencies, missingCurr) {
     
-    if (arr.length === 0) {
+    if (currencies.length === 0) {
         return ('Нет доступных валют');
     } 
-    function isMissingCurr(element, index, array) {
-        if (element == missingCurr) {
-           delete arr[index];
-        }
-    }
-    arr.find(isMissingCurr);
-    let result = 'Доступные валюты:\n';
 
-    arr.forEach((element) => {
-        result += element +'\n';
-    });
+
+    const availableCurrencies = currencies.filter((curr) => curr !== missingCurr);
+    const availableCurrenciesText = availableCurrencies.join('\n');
+    const result = `Доступные валюты:\n${availableCurrenciesText}`;
+
+    // function deleteIfCurrIsMissing(element, index) {
+    //     if (element == missingCurr) {
+    //        delete currencies[index];
+    //     }
+    // }
+
+    // currencies.forEach(deleteIfCurrIsMissing);
+
+    // let result = 'Доступные валюты:\n';
+
+    // currencies.forEach((element) => {
+    //     result += element +'\n';
+    // });
 
     return result;
 }
 
-console.log(availableCurr(arr, ''));
+console.log(availableCurr(allCurrencies, missingCurrency));
