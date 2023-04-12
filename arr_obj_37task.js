d1ca7ef4a0998b7b3532b23ab38a6ef58673fb45
momNatik <bas.n@list.ru>
2023-04-12 14:21:54 +0300

    feat: debugging (homework after 41 lesson)

--- arr_obj_37task.js
+++ arr_obj_37task.js
@@ -91,3 +91,15 @@ function sortStudentsByGroups(list, groupQuantity) {
 }
 
 console.log(sortStudentsByGroups(students, quantity));
+
+
+
+function foo(a,b) {
+    const [first] = a;
+    const {eng} = b;
+ 
+    return `${first} ${eng}`;
+}
+ 
+const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'})
+console.log(result);
