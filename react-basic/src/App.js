6afa89959907c2cf70f533566fad05ce63b71583
Nataliya <mom.natik@gmail.com>
2024-08-07 02:01:40 +0300

    components react

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world!</h2>
+}
+
+ 
+class Field extends React.Component {
+  render() {
+    const holder = 'Enter here'
+  const styledField = {
+    width: '300px'
+  }
+  return <input
+    type="text"
+    placeholder={holder}
+    style={styledField} />
+}
+  }
+
+
+function Btn() {
+  const text = 'Log in';
+
+  const logged = false;
+
+  // const res = () => {
+  //   return 'Log in!'
+  // }
+
+  // const p = <p>Log in</p>
+
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
 
+export {Header};
 export default App;
--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const text = 'Hello world!';
-
-const elem = (
-  <div>
-    <h2>Text: {text}</h2>
-    <input type='text'></input>
-    <button>Click</button>
-  </div>
-)
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-elem
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
