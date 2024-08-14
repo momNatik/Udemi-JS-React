ba301f46c0b73ff7cd0407b6f23f6f53d0c1b7bb
Nataliya <mom.natik@gmail.com>
2024-08-14 13:20:16 +0300

    +react-examples

---
+++ react-basic-examples/react-examples/src/App.js
@@ -0,0 +1,41 @@
+import {Component} from 'react';
+import './App.css';
+
+
+class WnoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27
+    }
+  }
+
+nextYear = () => {
+  this.setState({
+    years: this.state.years + 1
+  })
+}
+
+  render() {
+    const {name, surname, link} =this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>+++</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+     <WnoAmI name='Jonh' surname='Smith' link='facebook.com' />
+     <WnoAmI name='Alex' surname='Shepard' link='vk.com' />
+    </div>
+  );
+}
+
+export default App;
